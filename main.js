d30cb455eb066c5711b903b682f00dd752bedaf7
Shubham Jain <shubham.jain@hotmail.com>
2019-10-04 15:37:01 +0100

    Use more modern JS; Add pagination to SE API calls

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-const accounts = {};
-
-$('#go').click(function () {
-  const networkId = $('#networkId').val();
+function getPosts(user, site, page, cb) {
   const accessToken = $('#accessToken').val();
   const filter = '!0S2A5ipm(pczxWJLGNzYzyy4l';
   const key = 'c8aSGAR0rp5RUK)dVriyCA((';
 
-  $.getJSON(`http://api.stackexchange.com/2.2/users/${networkId}/associated`, function (j) {
+  $.getJSON(`https://api.stackexchange.com/2.2/users/${user}/posts?filter=${filter}&key=${key}&access_token=${accessToken}&site=${site}&page=${page}&pagesize=100`, j => {
+    for (let x = 0; x < j.items.length; x++) {
+      $('#posts').append(j.items[x].score + ' - <a href="' + j.items[x].link + '">' + j.items[x].title + '</a><br>');
+    }
+
+    if (j.has_more) getPosts(user, site, page + 1, () => console.log('Fetched more posts'));
+    else return cb();
+  });
+}
+
+$('#go').click(() => {
+  const accounts = {};
+  const networkId = $('#networkId').val();
+
+  $.getJSON(`http://api.stackexchange.com/2.2/users/${networkId}/associated`, j => {
     for (let i = 0; i < j.items.length; i++) {
       accounts[j.items[i]['site_url']] = j.items[i]['user_id'];
     }
 
-    $.each(accounts, function (k, v) {
+    $.each(accounts, (k, v) => {
       const site = k.replace(/http.?:\/\//, '');
-      $.getJSON(`https://api.stackexchange.com/2.2/users/${v}/posts?filter=${filter}&key=${key}&access_token=${accessToken}&site=${site}`, function (j2) {
-        for (let x = 0; x < j2.items.length; x++) {
-          $('#posts').append(j2.items[x]['score'] + ' - <a href="' + j2.items[x]['link'] + '">' + j2.items[x]['title'] + '</a><br>');
-        }
-      });
+      getPosts(v, site, 1, () => console.log('Fetched all posts'));
     });
   });
 });
